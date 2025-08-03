04973008efa792fce915cff3c7602aaa96ef1407
_MeePooh_ <122891690+PluemDontKnowToCode@users.noreply.github.com>
2025-08-03 12:28:37 +0700

    feat: Add Scene First time setting

---
+++ Assets/Map/FrameCode/SceneFirstTimeChecker.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneFirstTimeChecker : SingletonPersistent<SceneFirstTimeChecker>
+{
+    private  List<string> visitedScenes = new List<string>();
+
+    void Start()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if (!visitedScenes.Contains(sceneName))
+        {
+            Debug.Log($"First time visiting scene: {sceneName} in this session!");
+            visitedScenes.Add(sceneName);
+        }
+        else if (sceneName != "MainMenu" && visitedScenes.Contains(sceneName))
+        {
+            Debug.Log($"Already visited scene: {sceneName} before in this session.");
+            Mom mom = FindFirstObjectByType<Mom>();
+            if (!mom)
+                return;
+            mom.Respawn();
+        }
+    }
+
+}
